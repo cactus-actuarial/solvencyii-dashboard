df8b672846770b2c7da7a1fc7fa6d5897db19a6a
cactus <cactusactuarial@gmail.com>
2019-05-30 11:54:14 +0200

    ch... ch... changes

--- 2_4_ANOVA.R
+++ 2_4_ANOVA.R
@@ -3,29 +3,29 @@ library(ggplot2)
 set.seed(1)
 
 # d - dwarves
-# h - hobbits
-# m - all men
+# o - ogres
+# g - goblins
 
-m_d <- 202
-m_h <- 103
-m_m <- 163
+m_d <- 250
+m_o <- 83.9
+m_g <- 84.1
 
-sd_d <- 50
-sd_h <- 30
-sd_m <- 40
+sd_d <- 10
+sd_o <- 4
+sd_g <- 5
 
 # ls - life span
 
-sample_size = 5000
+sample_size = 1000
 
 ls_d <- round(rnorm(n = sample_size, mean = m_d, sd = sd_d), 0)
-ls_h <- round(rnorm(n = sample_size, mean = m_h, sd = sd_h), 0)
-ls_m <- round(rnorm(n = sample_size, mean = m_m, sd = sd_m), 0)
+ls_o <- round(rnorm(n = sample_size, mean = m_o, sd = sd_o), 0)
+ls_g <- round(rnorm(n = sample_size, mean = m_g, sd = sd_g), 0)
 
 # combine data
 
-life_span <- c(ls_d, ls_h, ls_m)
-race <- c(rep("dwarf", sample_size), rep("hobbit", sample_size), rep("all_men", sample_size))
+life_span <- c(ls_d, ls_o, ls_g)
+race <- c(rep("dwarf", sample_size), rep("ogre", sample_size), rep("goblin", sample_size))
 
 creatures <- data.frame(life_span, race)
 
@@ -33,13 +33,25 @@ head(creatures)
 summary(creatures)
 
 ggplot(creatures, aes(x = life_span, y = ..density..)) +
-    geom_histogram() +
-    facet_grid(race ~ .)
+    geom_histogram(binwidth = 1) +
+    facet_grid(race ~ .) +
+    scale_x_continuous(breaks = seq(from = 50, to = 290, by = 20))
+
+boxplot(life_span ~ race, data = creatures)
 
 # check normality
 
-shapiro.test(as.matrix(subset(creatures, race == "all_men", select = "life_span")))
 shapiro.test(as.matrix(subset(creatures, race == "dwarf", select = "life_span")))
-shapiro.test(as.matrix(subset(creatures, race == "hobbit", select = "life_span")))
+shapiro.test(as.matrix(subset(creatures, race == "ogre", select = "life_span")))
+shapiro.test(as.matrix(subset(creatures, race == "goblin", select = "life_span")))
 
-# one
+# one-way ANOVA
+
+oneway.test(life_span ~ race, data = creatures)
+
+oneway.test(life_span ~ race, data = subset(creatures, race != "dwarf"))
+
+# We can conclude that life expectancy varies significantly across races.
+
+# Kruskal-Wallis test
+kruskal.test(life_span ~ race, data = creatures)
